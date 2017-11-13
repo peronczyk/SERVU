587f85c1b328a6d573863ef6ebe7463f0fd9b00e
Bartosz Perończyk <bartosz@peronczyk.com>
2017-11-13 22:58:23 +0100

    Add ASCII ART in index.php :)

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
 /**
- * Hello to BROM, headless content management system.
+ *  ____  _____   ____  __  __
+ * |  _ \|  __ \ / __ \|  \/  |
+ * | |_) | |__) | |  | | \  / |
+ * |  _ <|  _  /| |  | | |\/| |
+ * | |_) | | \ \| |__| | |  | |
+ * |____/|_|  \_\\____/|_|  |_|
+ *
+ * HELLO TO BROM
+ * Headless Content Canagement System
+ *
+ * @author Bartosz Perończyk
+ *
  */
 
 define('BROM_START', microtime(true));
-
+define('BROM_VERSION', '0.0.1');
+define('BROM_INDEX', true);
 
 /**
  * Initiate core
@@ -20,6 +32,7 @@ $core = new Core();
  */
 
 $db = new Sqlite(_STORAGE_DIR . 'database/' . _DB_FILE_NAME, _DEBUG);
+
 
 echo '<pre>';
 
